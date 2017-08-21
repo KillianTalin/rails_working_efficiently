3cbd83ed68719cdcae34402529c757bd095663b9
Killian Talin <killian.talin@live.fr>
2017-08-21 16:48:16 +0200

    Creation de la homepage

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,9 +6,16 @@ class ProjectsController < ApplicationController
   end
 
   def new
+    # @project = Project.new
   end
 
   def create
+    # @project = Project.new(project_params)
+    # @project.user = current_user
+    #   if @project.save
+    #     redirect_to projects_path
+    #   else
+    #     render :new
   end
 
   def edit
@@ -18,5 +25,11 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :start_date, :end_date, :color, :avg_productivity_score, :total_worktime)
   end
 end
