2de06b1f6fcd8ae2e8adc30b465a138eddcbff1b
Killian Talin <killian.talin@live.fr>
2017-08-23 18:38:57 +0200

    Update

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,22 +1,22 @@
-window.addEventListener("load", function(event) {
-  initializeClock('clockdiv', start);
-});
+// window.addEventListener("load", function(event) {
+//   initializeClock('clockdiv', start);
+// });
 
-let start = new Date(0);
-function getTime(starttime) {
-  let t = starttime.setSeconds(starttime.getSeconds() + 1);
-  // console.log(t);
-  let seconds = Math.floor( (t/1000) % 60 );
-  let minutes = Math.floor( (t/1000/60) % 60 );
-  let hours = Math.floor( (t/(1000*60*60)) % 24 );
-  let days = Math.floor( t/(1000*60*60*24) );
-  return {
-    'total': t,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-};
+// let start = new Date(0);
+// function getTime(starttime) {
+//   let t = starttime.setSeconds(starttime.getSeconds() + 1);
+//   // console.log(t);
+//   let seconds = Math.floor( (t/1000) % 60 );
+//   let minutes = Math.floor( (t/1000/60) % 60 );
+//   let hours = Math.floor( (t/(1000*60*60)) % 24 );
+//   let days = Math.floor( t/(1000*60*60*24) );
+//   return {
+//     'total': t,
+//     'hours': hours,
+//     'minutes': minutes,
+//     'seconds': seconds
+//   };
+// };
 
 function initializeClock(id, starttime) {
   const clock = document.getElementById(id);
@@ -24,14 +24,14 @@ function initializeClock(id, starttime) {
   const minutesSpan = clock.querySelector('.minutes');
   const secondsSpan = clock.querySelector('.seconds');
   const buttonHiddenInput = document.querySelector("#task_elapsed_time");
-  function updateClock() {
-    let t = getTime(starttime);
-    console.log(t);
-    buttonHiddenInput.value = t.total / 1000;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  }
+  // function updateClock() {
+  //   let t = getTime(starttime);
+  //   console.log(t);
+  //   buttonHiddenInput.value = t.total / 1000;
+  //   hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+  //   minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+  //   secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  // }
 
   // updateClock(); // run function once at first to avoid delay
   // let timeinterval = setInterval(updateClock, 1000);
