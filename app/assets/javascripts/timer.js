ee19295b632fead419aef9ff34d9966017e10b63
Killian Talin <killian.talin@live.fr>
2017-08-23 18:41:02 +0200

    Update

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -24,14 +24,13 @@ function initializeClock(id, starttime) {
   const minutesSpan = clock.querySelector('.minutes');
   const secondsSpan = clock.querySelector('.seconds');
   const buttonHiddenInput = document.querySelector("#task_elapsed_time");
-  // function updateClock() {
-  //   let t = getTime(starttime);
-  //   console.log(t);
-  //   buttonHiddenInput.value = t.total / 1000;
-  //   hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-  //   minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-  //   secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-  // }
+  function updateClock() {
+    let t = getTime(starttime);
+    buttonHiddenInput.value = (t.total / 1000);
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  }
 
   // updateClock(); // run function once at first to avoid delay
   // let timeinterval = setInterval(updateClock, 1000);
