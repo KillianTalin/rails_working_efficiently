8bc5a357eac4d2315b6fc3523428d0ee1c5960d8
amin bhs <amin.diez@gmail.com>
2017-08-24 15:07:27 +0200

    nouvelles routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :projects do
-    resources :tasks, only: [:index]
+    resources :tasks, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   end
-  resources :tasks, only: [:show, :new, :create, :edit, :update, :destroy]
+  # resources :tasks, only: [:show, :new, :create, :edit, :update, :destroy]
   get '/tasks', to: 'tasks#index_direct'
 end
