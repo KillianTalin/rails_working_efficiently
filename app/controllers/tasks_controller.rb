fec0751e81b1bcdfc60eec04478b1f5c7523603c
amin bhs <amin.diez@gmail.com>
2017-08-24 13:54:35 +0200

    maj

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -68,7 +68,6 @@ class TasksController < ApplicationController
     @time = @tasks.sum(:real_duration)
     @projects = Project.where(user_id: current_user.id)
     @score = (@tasks.average(:productivity_score).round(2) / 5) * 100
-    @tags = @tasks.map { |task| {id: task.tag.id, name: task.tag.name}  }.uniq
   end
 
   private
